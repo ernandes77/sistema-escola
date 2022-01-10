33d4bd3f7c518dea0ffe0f8651251dd8f5eac9a9
Ernandes <ernandes77@hotmail.com>
2022-01-10 11:50:16 -0300

    Update StatusAluno.java

--- src/classesstatic/StatusAluno.java
+++ src/classesstatic/StatusAluno.java
@@ -1,5 +1,10 @@
 package classesstatic;
 
 public class StatusAluno {
-
-}
+	
+	public static String Aprovado = "ALUNO APROVADO";
+	public static String Recuperaco = "Aluno recuperacao";
+	public static String Reprovado = "AlunoReprovado";
+	
+	
+}              
